785f81e35b22fd895ede82b1d0c2f94b0b85c036
minh.nguyen <minh.nguyen@titancorpvn.com>
2017-05-05 17:31:35 +0700

    Minhnh commit to check data

---
+++ src/test/java/com/auvenir/ui/services/AuditorCreateToDoService.java
@@ -0,0 +1,61 @@
+package com.auvenir.ui.services;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+import com.auvenir.ui.pages.auditor.AuditorCreateToDoPage;
+import com.kirwa.nxgreport.NXGReports;
+import com.kirwa.nxgreport.logging.LogAs;
+import com.kirwa.nxgreport.selenium.reports.CaptureScreen;
+
+/**
+ * Created by hai.nguyen on 05/04/2017.
+ */
+
+public class AuditorCreateToDoService extends AbstractService {
+
+	AuditorCreateToDoPage createToDoPage;
+
+	/*
+	 * contructor
+	 */
+	public AuditorCreateToDoService(Logger logger, WebDriver driver) {
+
+		super(logger, driver);
+		createToDoPage = new AuditorCreateToDoPage(getLogger(), getDriver());
+
+	}
+
+	public void verifyAuditorCreateToDo() {
+
+		try {
+			this.createToDoPage.navigateToEngagementPage();
+			this.createToDoPage.navigateToToDoList();
+			//createToDoPage.verifyToDoListPage();
+			NXGReports.addStep("verify create to do page", LogAs.PASSED, null);
+		} catch (Exception e) {
+			NXGReports.addStep("verify create to do page", LogAs.FAILED,
+					new CaptureScreen(CaptureScreen.ScreenshotOf.BROWSER_PAGE));
+			//throw e;
+		}
+	}
+
+	public void verifyDataSearch(String searchData) throws Exception {
+		try {
+			boolean isCheckData = createToDoPage.checkSearchData(searchData);
+			if(isCheckData) {
+				NXGReports.addStep("verify search data on the to do page", LogAs.PASSED, null);
+			}
+			else
+			{
+				NXGReports.addStep("verify search data on the to do page", LogAs.FAILED, null);
+			}
+			System.out.println("isCheckData = " + isCheckData);
+		} catch (Exception e) {
+			NXGReports.addStep("verify search data on the to do page", LogAs.FAILED,
+					new CaptureScreen(CaptureScreen.ScreenshotOf.BROWSER_PAGE));
+		}
+	}
+	
+
+}
