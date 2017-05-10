142170eb31e5836086ff34a3e38630031f0d3fe6
Vien Pham <vien.pham@titancorpvn.com>
2017-05-10 15:49:39 +0700

    commit

---
+++ src/test/java/com/auvenir/ui/pages/auditor/AuditorCategoryPage.java
@@ -0,0 +1,51 @@
+package com.auvenir.ui.pages.auditor;
+
+import com.auvenir.ui.pages.common.AbstractPage;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import sun.awt.windows.WEmbeddedFrame;
+
+/**
+ * Created by vien.pham on 5/10/2017.
+ */
+public class AuditorCategoryPage extends AbstractPage {
+
+
+    public AuditorCategoryPage(Logger logger, WebDriver driver) {
+        super(logger, driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "category-dropdown")
+    WebElement eleCategoryMenu;
+
+    @FindBy(xpath = "//*[@id=\"category-dropdown-menu\"]/div[2]")
+    WebElement eleEditCategory;
+
+    @FindBy(xpath = "//*[@id=\"m-ce-systemContainer\"]/h3/text()")
+    WebElement eleCategoryTitle;
+
+    @FindBy(xpath = "//*[@id=\"edit-category-container\"]/div[1]")
+    WebElement eleCategoryGuide;
+
+    @FindBy(id = "cat-edit-btn")
+    WebElement eleCategoryPen;
+
+    @FindBy(id = "cat-trash-btn")
+    WebElement eleCategoryTrash;
+
+    @FindBy (id = "m-ce-cancelBtn")
+    WebElement eleCategoryCancelBtn;
+
+    @FindBy(id = "category-updateBtn")
+    WebElement eleCategorySaveBtn;
+
+
+
+
+
+
+}
---
+++ src/test/java/com/auvenir/ui/services/AuditorEditCategoryService.java
@@ -0,0 +1,42 @@
+package com.auvenir.ui.services;
+
+import com.auvenir.ui.pages.auditor.AuditorCategoryPage;
+import com.auvenir.ui.pages.auditor.AuditorCreateToDoPage;
+import com.auvenir.ui.pages.common.AbstractPage;
+import com.kirwa.nxgreport.NXGReports;
+import com.kirwa.nxgreport.logging.LogAs;
+import com.kirwa.nxgreport.selenium.reports.CaptureScreen;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by vien.pham on 5/10/2017.
+ */
+public class AuditorEditCategoryService extends AbstractService{
+
+    AuditorCategoryPage auditorCategoryPage;
+
+        /*
+     * contructor
+     */
+    public AuditorEditCategoryService(Logger logger, WebDriver driver) {
+
+        super(logger, driver);
+       // auditorCategoryPage = new AuditorCreateToDoPage(getLogger(), getDriver());
+    }
+
+    public void verifyCategoryGUI(){
+
+
+        try {
+
+            NXGReports.addStep("[PLAT 2288]-03: verify button create to do display with green background and white text.", LogAs.PASSED, null);
+        } catch (Exception e) {
+            NXGReports.addStep("[PLAT 2288]-03: verify button create to do display with green background and white text.", LogAs.FAILED,
+                    new CaptureScreen(CaptureScreen.ScreenshotOf.BROWSER_PAGE));
+        }
+    }
+
+
+
+}
