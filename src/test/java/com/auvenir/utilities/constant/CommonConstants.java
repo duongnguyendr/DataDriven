66fd4c75e0533394a783089e8b0f4395181a0c18
Doai Tran <doai.tran@titancorpvn.com>
2017-05-11 16:50:47 +0700

    delete commonConstants

--- src/test/java/com/auvenir/utilities/constant/CommonConstants.java
+++
@@ -1,8 +0,0 @@
-package com.auvenir.utilities.constant;
-
-/**
- * Created by tan.pham on 5/11/2017.
- */
-public class CommonConstants {
-    public static final String SCRIPT_FAIL = "Script Failed";
-}
