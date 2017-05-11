017f6c4fda884a5e7e2969b8bea9e9c9c3a09608
Tan Pham <tan.pham@titancorpvn.com>
2017-05-11 14:55:33 +0700

    add constants for test class

--- src/test/java/com/auvenir/ui/tests/auditor/AuditorSettingsTest.java
+++ src/test/java/com/auvenir/ui/tests/auditor/AuditorSettingsTest.java
@@ -1,6 +1,5 @@
 package com.auvenir.ui.tests.auditor;
 
-import com.auvenir.ui.services.AbstractRefactorService;
 import com.auvenir.ui.services.AbstractService;
 import com.auvenir.ui.services.AuditorEngagementService;
 import com.auvenir.ui.services.AuditorAccountSettingsService;
@@ -11,7 +10,7 @@ import com.kirwa.nxgreport.logging.LogAs;
 import com.kirwa.nxgreport.selenium.reports.CaptureScreen;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import com.auvenir.utilities.constant.*;
 /**
  * Created by cuong.nguyen on 4/27/2017.
  * Edited by Doai.Tran - PLAT 2273
@@ -22,12 +21,14 @@ public class AuditorSettingsTest extends AbstractTest {
     AuditorEngagementService auditorEngagementService;
 
 
-    @Test(priority=1,enabled=true, description="Verify Footer in Auditor Account Settings page.")
+    //@Test(priority=1,enabled=true, description="Verify Footer in Auditor Account Settings page.")
+    @Test(priority=1,enabled=true, description= TestConstants.VERIFY_FOOTER_AUDITOR_ACCOUNT_SETTING_PAGE)
     public void verifyFooterAuditorAccountSettingsPage() throws Exception
     {
         auditorAccountSettingsService = new AuditorAccountSettingsService(getLogger(),getDriver());
         auditorEngagementService = new AuditorEngagementService(getLogger(),getDriver());
-        String userId= GenericService.getCongigValue(GenericService.sConfigFile, "AUDITOR_ID");
+        //String userId= GenericService.getCongigValue(GenericService.sConfigFile, "AUDITOR_ID");
+        String userId= GenericService.getCongigValue(GenericService.sConfigFile, TestConstants.AUDITOR_ID_PARAMETER);
         try
         {
             auditorAccountSettingsService.loginWithUserRole(userId);
@@ -35,35 +36,43 @@ public class AuditorSettingsTest extends AbstractTest {
             auditorEngagementService.navigateToSettingsPage();
             auditorAccountSettingsService.verifyAccountSettingsPage();
             auditorAccountSettingsService.verifyFooter();
-            NXGReports.addStep("Verify Footer in Auditor Account Settings page.", LogAs.PASSED, null);
+            //NXGReports.addStep("Verify Footer in Auditor Account Settings page.", LogAs.PASSED, null);
+            NXGReports.addStep(TestConstants.VERIFY_FOOTER_AUDITOR_ACCOUNT_SETTING_PAGE_PASS, LogAs.PASSED, null);
         }
 
         catch (Exception e)
         {
-            NXGReports.addStep("Verify Footer in Auditor Account Settings page", LogAs.FAILED, new CaptureScreen(CaptureScreen.ScreenshotOf.BROWSER_PAGE));
+            //NXGReports.addStep("Verify Footer in Auditor Account Settings page", LogAs.FAILED, new CaptureScreen(CaptureScreen.ScreenshotOf.BROWSER_PAGE));
+            NXGReports.addStep(TestConstants.VERIFY_FOOTER_AUDITOR_ACCOUNT_SETTING_PAGE_FAIL, LogAs.FAILED, new CaptureScreen(CaptureScreen.ScreenshotOf.BROWSER_PAGE));
             throw e;
         }
     }
-    @Test(priority = 2,enabled = true,description = "Verify GUI auditor setting page.")
+    //@Test(priority = 2,enabled = true,description = "Verify GUI auditor setting page.")
+    @Test(priority = 2,enabled = true,description = TestConstants.VERIFY_GUI_AUDITOR_ACCOUNT_SETTING_PAGE)
     public void verifyUIAuditorSetting() throws Exception {
         auditorAccountSettingsService = new AuditorAccountSettingsService(getLogger(),getDriver());
         auditorEngagementService = new AuditorEngagementService(getLogger(),getDriver());
-        String userId= GenericService.getCongigValue(GenericService.sConfigFile, "AUDITOR_ID");
+        //String userId= GenericService.getCongigValue(GenericService.sConfigFile, "AUDITOR_ID");
+        String userId= GenericService.getCongigValue(GenericService.sConfigFile, TestConstants.AUDITOR_ID_PARAMETER);
         try{
             auditorAccountSettingsService.loginWithUserRole(userId);
             auditorEngagementService.verifyAuditorEngagementPage();
             auditorEngagementService.navigateToSettingsPage();
             auditorAccountSettingsService.verifyBody();
             auditorAccountSettingsService.verifyFooter();
-            Assert.assertTrue(AbstractService.sStatusCnt == 0, "Script Failed");
-            NXGReports.addStep("Verify GUI auditor setting page: PASSED", LogAs.PASSED, (CaptureScreen)null);
+            //Assert.assertTrue(AbstractService.sStatusCnt == 0, "Script Failed");
+            Assert.assertTrue(AbstractService.sStatusCnt == 0, TestConstants.SCRIPT_FAIL);
+            //NXGReports.addStep("Verify GUI auditor setting page: PASSED", LogAs.PASSED, (CaptureScreen)null);
+            NXGReports.addStep(TestConstants.VERIFY_GUI_AUDITOR_ACCOUNT_SETTING_PAGE_PASS, LogAs.PASSED, null);
         }catch (Exception e)
         {
-            NXGReports.addStep("Verify All UI Audit Settings page", LogAs.FAILED, new CaptureScreen(CaptureScreen.ScreenshotOf.BROWSER_PAGE));
+            //NXGReports.addStep("Verify All UI Audit Settings page", LogAs.FAILED, new CaptureScreen(CaptureScreen.ScreenshotOf.BROWSER_PAGE));
+            NXGReports.addStep(TestConstants.VERIFY_GUI_AUDITOR_ACCOUNT_SETTING_PAGE_FAIL, LogAs.FAILED, new CaptureScreen(CaptureScreen.ScreenshotOf.BROWSER_PAGE));
             throw e;
         }
     }
-    @Test(priority = 3,enabled = true,description = "Test First and Last name on Auditor Setting Page.")
+    //@Test(priority = 3,enabled = true,description = "Test First and Last name on Auditor Setting Page.")
+    @Test(priority = 3,enabled = true,description = TestConstants.TEST_FIRST_LAST_NAME_FIELD_IN_ACCOUNT_SETTING_PAGE)
     public void InputValueFullName() throws Exception {
         auditorAccountSettingsService = new AuditorAccountSettingsService(getLogger(),getDriver());
         auditorEngagementService = new AuditorEngagementService(getLogger(),getDriver());
---
+++ src/test/java/com/auvenir/utilities/constant/TestConstants.java
@@ -0,0 +1,22 @@
+package com.auvenir.utilities.constant;
+
+/**
+ * Created by tan.pham on 5/11/2017.
+ */
+public class TestConstants {
+
+    //Common
+    public static final String SCRIPT_FAIL = "Script Failed";
+
+    // AuditorSettingsTest class
+    public static final String VERIFY_FOOTER_AUDITOR_ACCOUNT_SETTING_PAGE = "Verify Footer in Auditor Account Settings page.";
+    public static final String VERIFY_FOOTER_AUDITOR_ACCOUNT_SETTING_PAGE_PASS = "Verify Footer in Auditor Account Settings page : PASSED.";
+    public static final String VERIFY_FOOTER_AUDITOR_ACCOUNT_SETTING_PAGE_FAIL = "Verify Footer in Auditor Account Settings page";
+    public static final String VERIFY_GUI_AUDITOR_ACCOUNT_SETTING_PAGE = "Verify GUI in Auditor Account Settings page.";
+    public static final String VERIFY_GUI_AUDITOR_ACCOUNT_SETTING_PAGE_PASS = "Verify GUI Auditor Account Settings page : PASSED.";
+    public static final String VERIFY_GUI_AUDITOR_ACCOUNT_SETTING_PAGE_FAIL = "Verify GUI in Auditor Account Settings page";
+    public static final String AUDITOR_ID_PARAMETER = "AUDITOR_ID";
+
+    public static final String TEST_FIRST_LAST_NAME_FIELD_IN_ACCOUNT_SETTING_PAGE = "Test First and Last name in Auditor Setting Page.";
+
+}
