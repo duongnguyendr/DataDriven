75d0c22514785a24c79e4811e5a8210e4f15c7e4
Tan Pham <tan.pham@titancorpvn.com>
2017-05-22 14:21:39 +0700

    add new verifyGUIPopUpComplete

--- src/test/java/com/auvenir/ui/pages/common/PopUpPage.java
+++ src/test/java/com/auvenir/ui/pages/common/PopUpPage.java
@@ -27,11 +27,16 @@ public class PopUpPage extends AbstractPage {
     static final  String DELETE_BUTTON_COLOR = "rgba(255, 255, 255, 1)";
     static final  String DELETE_BUTTON_BACK_GROUND_COLOR = "rgba(241, 103, 57, 1)";
 
+    static final  String ARCHIVE_BUTTON_TITLE = "archive button";
+    static final  String ARCHIVE_BUTTON_COLOR = "rgba(255, 255, 255, 1)";
+    static final  String ARCHIVE_BUTTON_BACK_GROUND_COLOR = "rgba(89, 155, 161, 1)";
+
     static final  String CANCEL_BUTTON_TITLE = "cancel button";
     static final  String CANCEL_BUTTON_COLOR = "rgba(255, 255, 255, 1)";
     static final  String CANCEL_BUTTON_BACK_GROUND_COLOR = "rgba(151, 147, 147, 1)";
 
     static final  String POP_UP_DELETE_TITLE_TEXT = "Delete To-Do?";
+    static final  String POP_UP_ARCHIVE_TITLE_TEXT = "Mark As Complete?";
     static final  String POP_UP_DELETE_GUIDE_SENTENCE_TEXT = "Are you sure you'd like to delete these To-Dos? " +
                                                              "Once deleted, you will not be able to retrieve any documents uploaded to the selected To-Dos.";
 
@@ -47,8 +52,6 @@ public class PopUpPage extends AbstractPage {
     static final String COLOR_BUTTON_FIRST_MESSAGE_SUCCESS = "Color of ";
     static final String COLOR_BUTTON_LAST_MESSAGE_SUCCESS = " is show correct.";
 
-
-
     //public constructor
     public PopUpPage(Logger logger, WebDriver driver)
     {
@@ -56,6 +59,14 @@ public class PopUpPage extends AbstractPage {
         PageFactory.initElements(driver, this);
     }
 
+    /**
+     * Verify GUI of popup delete
+     * @param elePopUpTile : popup title element
+     * @param eleGuideSentenceTitle : guide sentence element
+     * @param eleCancelButton : cancel button element
+     * @param eleDeleteButton : delete button element
+     * @return true : All of GUI is complete | false : All of GUI is not complete
+     */
     public boolean verifyGUIPopUpDelete(WebElement elePopUpTile,WebElement eleGuideSentenceTitle,
                                         WebElement eleCancelButton, WebElement eleDeleteButton){
         if(verifyGUIPopUp(elePopUpTile,POP_UP_DELETE_TITLE_TEXT,
@@ -65,6 +76,41 @@ public class PopUpPage extends AbstractPage {
         return true;
     }
 
+    /**
+     * Verify GUI of popup complete
+     * @param elePopUpTile : popup title element
+     * @param eleGuideSentenceTitle : guide sentence title element
+     * @param guideSentence : guide sentence description
+     * @param eleCancelButton : cancel button element
+     * @param eleArchiveButton : archive button element
+     * @return true : All of GUI is complete | false : All of GUI is not complete
+     */
+    public boolean verifyGUIPopUpComplete(WebElement elePopUpTile,WebElement eleGuideSentenceTitle,
+                                          String guideSentence,WebElement eleCancelButton, WebElement eleArchiveButton){
+        if(verifyGUIPopUp(elePopUpTile,POP_UP_ARCHIVE_TITLE_TEXT,
+                eleGuideSentenceTitle,guideSentence,
+                eleCancelButton, CANCEL_BUTTON_TITLE,CANCEL_BUTTON_COLOR,CANCEL_BUTTON_BACK_GROUND_COLOR,
+                eleArchiveButton, ARCHIVE_BUTTON_TITLE,ARCHIVE_BUTTON_COLOR,ARCHIVE_BUTTON_BACK_GROUND_COLOR));
+        return true;
+    }
+
+
+    /**
+     * Verify GUI of popup common
+     * @param elePopUpTile : popup title element
+     * @param popUpTitleDescription : popup title description
+     * @param eleGuideSentenceTitle : guide sentence element
+     * @param guideSentenceTitleDescription : guide sentence description
+     * @param eleOneButton : Cancel button element.
+     * @param txtOneButton : text of cancel button
+     * @param txtOneButtonColor : color text of cancel button
+     * @param txtOneButtonBackGround : background-color of cancel button
+     * @param eleTwoButton : Delete/Archive element
+     * @param txtTwoButton : text of delete/archive button
+     * @param txtTwoButtonColor : color text of delete/archive button
+     * @param txtTwoButtonBackGround : background-color of delete/archive button
+     * @return true : All every thing in GUI ok | false : Any of every thing in GUI is not ok
+     */
     public boolean verifyGUIPopUp(WebElement elePopUpTile, String popUpTitleDescription,
                                   WebElement eleGuideSentenceTitle, String guideSentenceTitleDescription,
                                   WebElement eleOneButton, String txtOneButton, String txtOneButtonColor, String txtOneButtonBackGround,
