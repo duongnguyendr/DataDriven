1adfca6b03a4f54a503f3dd9a86d58b53110e3a6
Tan Pham <tan.pham@titancorpvn.com>
2017-05-29 16:10:16 +0700

    Refactory CareerPage --> CareerRefactoryTest.java

---
+++ src/test/java/com/auvenir/ui/services/CareerService.java
@@ -0,0 +1,44 @@
+package com.auvenir.ui.services;
+
+import com.auvenir.ui.pages.AuvenirPage;
+import com.auvenir.ui.pages.CareerPage;
+import com.auvenir.ui.pages.auditor.AuditorCreateToDoPage;
+import com.auvenir.ui.pages.auditor.AuditorDetailsEngagementPage;
+import com.auvenir.ui.pages.auditor.AuditorTodoListPage;
+import com.auvenir.utilities.GenericService;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by tan.pham on 5/29/2017.
+ */
+public class CareerService extends AbstractService {
+
+    CareerPage careerPage;
+    AuvenirPage auvenirPage;
+
+    /*
+     * contructor
+     */
+    public CareerService(Logger logger, WebDriver driver) {
+        super(logger, driver);
+        careerPage = new CareerPage(getLogger(), getDriver());
+        auvenirPage = new AuvenirPage(getLogger(), getDriver());
+    }
+
+    public void clickOnCareerLink(){
+        auvenirPage.clickOnCareerLink();
+    }
+
+    public void verifyFooterCareerPathPage(){
+        careerPage.verifyFooterCareerPathPage();
+    }
+
+    public void verifyContenCareerPathPage(){
+        careerPage.verifyContenCareerPathPage();
+    }
+    public void verifyHeaderCareerPathPage(){
+        careerPage.verifyHeaderCareerPathPage();
+    }
+
+}
