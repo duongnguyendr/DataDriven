c7f1e4d40744339fae62a0c081e41d83db2d740e
duong.nguyen <duong.nguyen@titancorpvn.com>
2017-06-08 15:52:39 +0700

    Remove unuse import

--- src/test/java/com/auvenir/ui/pages/auditor/AuditorEngagementPage.java
+++ src/test/java/com/auvenir/ui/pages/auditor/AuditorEngagementPage.java
@@ -3,16 +3,16 @@ package com.auvenir.ui.pages.auditor;
 import com.auvenir.ui.pages.AuvenirPage;
 import com.auvenir.ui.pages.common.AbstractPage;
 import com.auvenir.utilities.GeneralUtilities;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-//import org.testng.log4testng.Logger;
-import org.apache.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
+
+//import org.testng.log4testng.Logger;
 
 public class AuditorEngagementPage extends AbstractPage {
 
