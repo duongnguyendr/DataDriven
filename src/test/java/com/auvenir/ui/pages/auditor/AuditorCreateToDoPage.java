34f37b0c71fa26e3124ff9939b07cd6a4d31b260
Thuan Duong <thuan.duong@titancorpvn.com>
2017-06-23 11:58:30 +0700

    Update libraries for function waiting animation

--- src/test/java/com/auvenir/ui/pages/auditor/AuditorCreateToDoPage.java
+++ src/test/java/com/auvenir/ui/pages/auditor/AuditorCreateToDoPage.java
@@ -11,12 +11,11 @@ import com.kirwa.nxgreport.selenium.reports.CaptureScreen;
 import com.mongodb.DBCollection;
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import javax.sql.rowset.spi.SyncFactoryException;
