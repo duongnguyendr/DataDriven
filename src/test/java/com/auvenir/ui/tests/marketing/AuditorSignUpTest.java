48a0314e7e6ff3dd3827bfa2c1439e4256efcdb4
Thuan Duong <thuan.duong@titancorpvn.com>
2017-06-26 11:39:29 +0700

    Fix element xpath for AdminLoginPage.

--- src/test/java/com/auvenir/ui/tests/marketing/AuditorSignUpTest.java
+++ src/test/java/com/auvenir/ui/tests/marketing/AuditorSignUpTest.java
@@ -79,7 +79,6 @@ public class AuditorSignUpTest extends AbstractTest {
             // This test cases is verified creating new user.
             // It must be deleted old user in database before create new one.
             // Below comment code need to be verify with new environment due to the business rule is changed.
-            System.out.println("emailCreate: " + emailCreate);
             auditorSignUpService.deleteUserUsingApi(emailCreate);
             auditorSignUpService.deleteUserUsingMongoDB(emailCreate);
 
