06aa34a623f2edd0e9e2d65cc2ccc78b99c8018a
Vien Pham <vien.pham@titancorpvn.com>
2017-07-04 19:38:01 +0700

    Vienpham commit code

---
+++ src/test/java/com/auvenir/ui/services/auditor/AuditorContactService.java
@@ -0,0 +1,35 @@
+package com.auvenir.ui.services.auditor;
+
+/**
+ * Created by tan.pham on 6/30/2017.
+ */
+import com.auvenir.ui.pages.auditor.AuditorAcountSettingsPage;
+import com.auvenir.ui.pages.auditor.AuditorContactsPage;
+import com.auvenir.ui.services.AbstractService;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+public class AuditorContactService extends AbstractService{
+    AuditorContactsPage auditorContactsPage;
+    /*
+     * contructor
+     */
+    public AuditorContactService(Logger logger, WebDriver driver) {
+
+        super(logger, driver);
+        auditorContactsPage = new AuditorContactsPage(getLogger(), getDriver());
+
+    }
+
+    public void verifyAuditorContactPage(){
+        auditorContactsPage.verifyAuditorContactsPage();
+    }
+
+    public void clickOnEngagementLink(){
+        auditorContactsPage.clickOnEngagementLink();
+    }
+
+    public void verifyGUIDataTableContactPage(){
+        auditorContactsPage.verifyGUIDataTableInContactPage();
+    }
+}
