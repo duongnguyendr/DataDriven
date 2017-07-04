2ae262916bb971f1a487cbefaaa54cb943930d8c
duong.nguyen <duong.nguyen@titancorpvn.com>
2017-07-04 16:21:54 +0700

    edit abstractTest

--- src/test/java/com/auvenir/utilities/listeners/TestngListener.java
+++ src/test/java/com/auvenir/utilities/listeners/TestngListener.java
@@ -39,10 +39,9 @@ public class TestngListener implements ITestListener {
 		Date date = new Date();
 	    SimpleDateFormat sdtf=new SimpleDateFormat("dd-MM-yyyy_hh_mm_ss");
 	    sdateTime = sdtf.format(date);
-		sHtmlReports=new File(GenericService.sDirPath+"//Reports//HTMLReports");
-		System.out.println("HTML Report folder: " + sHtmlReports.getName());
-		sTestngReports= new File(GenericService.sDirPath+"//Reports//TestNGReports");
-		sPdfReports = new File(GenericService.sDirPath+"//Reports//PDFReports");
+		sHtmlReports=new File(GenericService.sDirPath+"//..//Reports//HTMLReports");
+		sTestngReports= new File(GenericService.sDirPath+"//..//Reports//TestNGReports");
+		sPdfReports = new File(GenericService.sDirPath+"//..//Reports//PDFReports");
 		if(!sHtmlReports.exists())
 		{
 			FileUtils.forceMkdir(sHtmlReports);		
@@ -99,13 +98,9 @@ public class TestngListener implements ITestListener {
 
 	public void onFinish(ITestContext context) 
 	{
-		 /*File testOuput = new File(GenericService.sDirPath+"\\tests-output");
+		 File testOuput = new File(GenericService.sDirPath+"\\tests-output");
 		 String sTestngReports= GenericService.sDirPath+"\\..\\Reports\\TestNGReports\\TestNG_"+sdateTime;
-		 pdfReports = new File(sPdfReports+"\\PDFReports"+sdateTime+".pdf");*/
-		File testOuput = new File(GenericService.sDirPath+"\\tests-output");
-		String sTestngReports= GenericService.sDirPath+"\\Reports\\TestNGReports\\TestNG_"+sdateTime;
-		System.out.println("Report folder: " + sTestngReports);
-		pdfReports = new File(sPdfReports+"\\PDFReports"+sdateTime+".pdf");
+		 pdfReports = new File(sPdfReports+"\\PDFReports"+sdateTime+".pdf");
 		 iPassCount=context.getPassedTests().size();
 	     iFailCount=context.getFailedTests().size(); 
 	     iSkippedCount=context.getSkippedTests().size(); 
