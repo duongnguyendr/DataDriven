f5a021bf8bd43f77bc18589a350e4038de583fcc
duong.nguyen <duong.nguyen@titancorpvn.com>
2017-07-04 18:15:12 +0700

    edit abstractTest

--- src/test/java/com/auvenir/utilities/htmlreport/com/nxgreport/utils/TestDirectory.java
+++ src/test/java/com/auvenir/utilities/htmlreport/com/nxgreport/utils/TestDirectory.java
@@ -43,6 +43,7 @@ public class TestDirectory {
 
     static {
         REPORTSDIR = CURRENTDIR + SEP + REPORTSDIRNAME;
+        System.out.println("**********REPORTSDIR: " + REPORTSDIR);
         RESOURCESDIRName = "Resources";
         RESOURCESDIR = REPORTSDIR + SEP + RESOURCESDIRName;
         CSSDIRName = "Css";
