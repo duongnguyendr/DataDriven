f6a7967ec75a0b615b54d1f3fff5d69f0c2c489e
duong.nguyen <duong.nguyen@titancorpvn.com>
2017-07-17 14:16:07 +0700

    Create LeadAuditorTest class

---
+++ src/test/java/com/auvenir/ui/tests/groupPermissions/LeadAuditorTest.java
@@ -0,0 +1,16 @@
+package com.auvenir.ui.tests.groupPermissions;
+
+import com.auvenir.ui.tests.AbstractTest;
+import org.testng.annotations.Test;
+
+/**
+ * Created by duong.nguyen on 7/17/2017.
+ */
+public class LeadAuditorTest extends AbstractTest {
+
+
+    @Test(priority = 27, enabled = true, description = "Verify lead auditor can see all files within an engagement.")
+    public void verifyLeadAuditorCanSeeAllFileRequest() throws Exception {
+
+    }
+}
