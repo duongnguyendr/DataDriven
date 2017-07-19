68ad0718cb0505204866c51f6ceac5ab4fd551d3
HuyHuynh <huy.huynh@titancorpvn.com>
2017-07-19 10:26:40 +0700

    Add test and service files

---
+++ src/test/java/com/auvenir/ui/tests/groupPermissions/GroupPermissionsInitialTest.java
@@ -0,0 +1,10 @@
+package com.auvenir.ui.tests.groupPermissions;
+
+import com.auvenir.ui.tests.AbstractTest;
+
+/**
+ * Created by huy.huynh on 19/07/2017.
+ */
+public class GroupPermissionsInitialTest extends AbstractTest {
+
+}
