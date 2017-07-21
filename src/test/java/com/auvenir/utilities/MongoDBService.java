b1a376af8e926843e198f1d389f4115aa738154e
Thuan Duong <thuan.duong@titancorpvn.com>
2017-07-21 15:15:00 +0700

    Update new xpath for element titleAllEngagment, create new overload function removeEngagementCreatedByLeadAuditor

--- src/test/java/com/auvenir/utilities/MongoDBService.java
+++ src/test/java/com/auvenir/utilities/MongoDBService.java
@@ -868,6 +868,49 @@ public class MongoDBService {
             if(objectId != null) {
                 BasicDBObject searchQuery = new BasicDBObject();
                 searchQuery.put("acl.id", new ObjectId(objectId));
+                BasicDBObject searchQuery1 = new BasicDBObject();
+                searchQuery.put("name",engagementName);
+                DBCursor curs = dBCollection.find(searchQuery, searchQuery1);
+                while (curs.hasNext()) {
+                    DBObject dBbject = curs.next();
+                    // shows the whole result document
+                    BasicDBList aclObjectID = (BasicDBList) dBbject.get("acl");
+                    BasicDBObject[] aclObjectArr = aclObjectID.toArray(new BasicDBObject[0]);
+                    for(BasicDBObject dbObj : aclObjectArr) {
+                        // shows each item from the lights array
+                        if(dbObj.get("id").toString().equals(objectId) && dbObj.get("lead").equals(true)) {
+                            System.out.println("Engagement ObjectID: " + dBbject.get("_id"));
+                            System.out.println("Acl ObjectID: " + dBbject.get("acl"));
+                            dBCollection.remove(dBbject);
+                        }
+                    }
+                    count++;
+                }
+            }
+            if (count == 0) {
+                System.out.println(String.format("Engagement name '%s' is created by email '%s' is not exist on database", engagementName, email));
+            } else
+                System.out.println("Deleted Engagement successfully.");
+        } catch (NoSuchElementException ex) {
+            System.out.println("This engagement not exist on database.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Remove all engagement that created by Auditor(email)
+     * @param email the String email which has object ID displayed in 'acl' array object.
+     */
+    public static void removeEngagementCreatedByLeadAuditor(String email) {
+        String objectId = null;
+        int count = 0;
+        try {
+            DBCollection dBCollection = getCollection("engagements");
+            objectId = getObjectIdOfEmailUser(email);
+            if(objectId != null) {
+                BasicDBObject searchQuery = new BasicDBObject();
+                searchQuery.put("acl.id", new ObjectId(objectId));
 //                BasicDBObject searchQuery1 = new BasicDBObject();
 //                searchQuery.put("name",engagementName);
                 DBCursor curs = dBCollection.find(searchQuery);
@@ -888,7 +931,7 @@ public class MongoDBService {
                 }
             }
             if (count == 0) {
-                System.out.println(String.format("Engagement name '%s' is created by email '%s' is not exist on database", engagementName, email));
+                System.out.println(String.format("User '%s' does not have any engagement on database", email));
             } else
                 System.out.println("Deleted Engagement successfully.");
         } catch (NoSuchElementException ex) {
